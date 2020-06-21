fe43ae37c10981a5291beddf6678ccbb15c52523
Harsh Kailash <harshkailash2000@gmail.com>
2020-06-21 23:19:46 +0530

    Create add.php

---
+++ add update delete/add.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>add item</title>
+</head>
+<body>
+<form method="POST" name="form1" action="add_process.php">
+product id <input type="text" name="pid"><br><br>
+product name <input type="text" name="pname"><br><br>
+description <input type="textarea" name="des"><br><br>
+cost <input type="text" name="cst"><br><br>
+quantity <input type="number" name="qty"><br><br>
+<input type="submit" name="submit" value="Add Product">
+
+</form>
+</body>
+</html>
