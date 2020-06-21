2383cc1f974e5acff1ecf1f7d0a909d65e743a0c
Harsh Kailash <harshkailash2000@gmail.com>
2020-06-21 23:29:59 +0530

    Add files via upload

---
+++ admin and user/admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="POST" name="form1" action="add.php">
+	<input type="submit" name="add" value="add"><br>
+	click this button to add a product
+	<br><br><br><br>
+	</form>
+	<form method="POST" name="form2" action="update.php">
+	<input type="submit" name="update" value="update"><br>
+	click this button to add a product
+	<br><br><br><br>
+	</form>
+	<form method="POST" name="form1" action="del.php">
+	<input type="submit" name="del" value="delete"><br>
+	click this button to add a product
+	<br><br><br><br>
+	</form>
+
+</body>
+</html>
