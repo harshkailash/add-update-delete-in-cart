0cf1be77ae56b2e9f42f2d11a09e1edbcee46107
Harsh Kailash <harshkailash2000@gmail.com>
2020-06-21 23:27:29 +0530

    Create login.php

---
+++ login and registration/login.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="loginstyle.css">
+	<title>Login page</title>
+</head>
+<body>
+<form method="POST" name="form1" action="login_entry.php">
+	<table border="2" cellpadding="10px" cellspacing="10px" align="center">
+	<tbody>
+	<tr><td>Username</td><td><input type="text" name="uid" required></input></td></tr>
+		<tr><td>Password</td><td><input type="password" name="pwd" required></input></td></tr>
+		<tr><td colspan="2" align="center"><input type="submit" name="s"></td></tr>
+	</tbody>
+	
+</table>
+</form>
+</body>
+</html>
